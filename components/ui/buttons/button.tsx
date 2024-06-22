5f55981859df17d90cfcf66e6dfcd9e1f5b52e5f
Jaco Kotzee <jakwakwa@gmail.com>
2024-06-22 05:19:19 +0200

    addboard button text variant color fix

--- components/ui/buttons/button.tsx
+++ components/ui/buttons/button.tsx
@@ -20,8 +20,8 @@ const Button = ({
           className={`${isDisabled && "bg-kpurple-light cursor-not-allowed"} ${
             variant
               ? "bg-white border border-indigo-600 text-indigo-600 text-xs hover:bg-slate-100 transition-colors ease-in-out duration-75  px-2 py-1"
-              : "bg-kpurple-main hover:bg-slate-500  px-5 py-3"
-          } rounded-3xl text-md text-white text-sm font-semibold `}
+              : "bg-kpurple-main hover:bg-slate-500 text-white px-5 py-3"
+          } rounded-3xl text-md text-sm font-semibold `}
         >
           {children}
         </button>
