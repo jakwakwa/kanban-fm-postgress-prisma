962a4729ac73e053bfe1003c577d31fe68d89e4b
Jaco Kotzee <jakwakwa@gmail.com>
2024-06-23 19:02:12 +0200

    Readonly props

--- app/kanban/board/layout.tsx
+++ app/kanban/board/layout.tsx
@@ -1,4 +1,8 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="flex   flex-col md:flex-rReadonly<{ children: React.ReactNode }>F7FD] h-fit overflow-scroll">
       <div className="w-[20000px] flex-grow p-6 md:overflow-y-auto md:p-0">
